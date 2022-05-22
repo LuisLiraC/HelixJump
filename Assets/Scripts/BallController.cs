537f2ef5b3e81e7464a98bd8a23846b01d22e5e0
LuisLiraC <luis_ssbm@hotmail.com>
2022-05-22 03:08:24 -0500

    4.1. Una propuesta en 3D

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float impulseForce = 3f;
+
+    private bool ignoreNextCollision;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (ignoreNextCollision) return;
+
+        rb.velocity = Vector3.zero;
+        rb.AddForce(Vector3.up * impulseForce, ForceMode.Impulse);
+
+        ignoreNextCollision = true;
+        Invoke("AllowNextCollision", 0.2f);
+    }
+
+    private void AllowNextCollision()
+    {
+        ignoreNextCollision = false;
+    }
+}
---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    [SerializeField] BallController ball;
+    private float offset;
+    void Start()
+    {
+        offset = transform.position.y - ball.transform.position.y;
+    }
+
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        currentPosition.y = ball.transform.position.y + offset;
+        transform.position = currentPosition;
+    }
+}
